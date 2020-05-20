c60104a3288ad7e5a13d8965f20f48438347a411
cypark728 <48200520+cypark728@users.noreply.github.com>
2020-05-20 11:41:18 +0900

    2020-05-20

---
+++ 2020-05/10039.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    int a;
+    int sum = 0;
+    for(int i=0;i<5;i++){
+        scanf("%d", &a);
+        if(a<40){
+            a = 40;
+        }
+        sum = sum + a;
+    }
+    printf("%d", sum / 5);
+
+
+    return 0;
+}
